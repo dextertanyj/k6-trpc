9557ec997364181c3d033435f099428e11e8e842
Dexter Tan <dev@dextertanyj.com>
2023-08-21 20:58:21 +0800

    feat: migrate to proxy implementation

--- src/index.ts
+++ src/index.ts
@@ -1,117 +1,173 @@
-import { type AnyProcedure, type AnyRouter } from "@trpc/server";
-import SuperJSON from "superjson";
+import {
+  type AnyProcedure,
+  type AnyRouter,
+  type CombinedDataTransformer,
+  type DataTransformer,
+  defaultTransformer,
+} from "@trpc/server";
 
-import { get, post } from "k6/http";
+import http, { type get, type Params, type post } from "k6/http";
 
-// We need this to polyfill the URL class that is used in SuperJSON.
+// We need this to polyfill the URL class.
 require("https://jslib.k6.io/url/1.0.0/index.js");
 
-export const NONE = Symbol();
+const NONE = Symbol();
+const OPTION = Symbol();
 
-type GetRouterKeys<T extends AnyRouter> = Exclude<keyof T, keyof AnyRouter>;
+export function options(options: Omit<Params, "responseCallback">) {
+  return { [OPTION]: "OPTION", ...options };
+}
 
-type GetProcedureType<T extends AnyProcedure> = T["_type"];
+type RouterKeys<T extends AnyRouter> = Exclude<keyof T, keyof AnyRouter>;
 
-type ProcedurePaths<
-  R extends AnyRouter,
-  T extends GetProcedureType<AnyProcedure>,
-> = GetRouterKeys<R> extends infer K
-  ? K extends string & keyof R
-    ? R[K] extends AnyProcedure
-      ? GetProcedureType<R[K]> extends T
-        ? K
+type ProcedureType<T extends AnyProcedure> = T["_type"];
+
+type InputType<P extends AnyProcedure> = P["_def"]["_input_in"] extends infer input
+  ? input extends symbol
+    ? typeof NONE
+    : input
+  : never;
+
+type Handler<R extends AnyRouter, K extends keyof R> = R[K] extends AnyRouter
+  ? Client<R[K]>
+  : R[K] extends AnyProcedure
+  ? ProcedureType<R[K]> extends infer T
+    ? InputType<R[K]> extends infer I
+      ? T extends "query"
+        ? {
+            query: I extends typeof NONE
+              ? (opts?: ReturnType<typeof options>) => ReturnType<typeof get>
+              : (input: I, opts?: ReturnType<typeof options>) => ReturnType<typeof get>;
+          }
+        : T extends "mutation"
+        ? {
+            mutate: I extends typeof NONE
+              ? (opts?: ReturnType<typeof options>) => ReturnType<typeof post>
+              : (input: I, opts?: ReturnType<typeof options>) => ReturnType<typeof post>;
+          }
         : never
       : never
     : never
   : never;
 
-type RouterPaths<R extends AnyRouter> = GetRouterKeys<R> extends infer K
-  ? K extends string & keyof R
-    ? R[K] extends AnyRouter
-      ? RouterPaths<R[K]> extends infer Child
-        ? Child extends string
-          ? Child extends ""
-            ? K
-            : `${K}.${Child}`
-          : never
-        : never
-      : R[K] extends AnyProcedure
-      ? ""
-      : never
-    : never
-  : never;
+export type Client<R extends AnyRouter> = {
+  [K in RouterKeys<R>]: Handler<R, K>;
+};
 
-type GetInputType<P extends string, R extends AnyRouter> = P extends `${infer K}.${infer Rest}`
-  ? K extends keyof R
-    ? R[K] extends AnyRouter
-      ? GetInputType<Rest, R[K]>
-      : never
-    : never
-  : P extends keyof R
-  ? R[P] extends infer T
-    ? T extends AnyProcedure
-      ? T["_def"]["_input_in"] extends infer input
-        ? input extends symbol
-          ? typeof NONE
-          : input
-        : never
-      : never
-    : never
-  : never;
+const isCombinedDataTransformer = (
+  transformer: CombinedDataTransformer | DataTransformer,
+): transformer is CombinedDataTransformer => {
+  return Object.prototype.hasOwnProperty.call(transformer, "input");
+};
 
-type Router<R extends AnyRouter, P extends RouterPaths<R>> = P extends `${infer K extends
-  string}.${infer Rest}`
-  ? K extends keyof R
-    ? R[K] extends AnyRouter
-      ? Rest extends RouterPaths<R[K]>
-        ? Router<R[K], Rest>
-        : never
-      : never
-    : never
-  : P extends keyof R
-  ? R[P] extends AnyRouter
-    ? R[P]
-    : never
-  : P extends ""
-  ? R
-  : never;
+const isOptions = (opts: unknown): opts is ReturnType<typeof options> => {
+  return (
+    typeof opts === "object" && opts !== null && Object.prototype.hasOwnProperty.call(opts, OPTION)
+  );
+};
 
-export const createClient = <R extends AnyRouter>(url: string) => {
-  const createRouter = <P extends RouterPaths<R>>(path: P) => {
-    const routerURL = new URL(path, url).toString();
+const createFunctionHandler = (
+  url: string,
+  property: string,
+  transformer: CombinedDataTransformer | DataTransformer,
+) => {
+  return (...args: unknown[]) => {
+    let opts: ReturnType<typeof options> | undefined;
+    let input: unknown = NONE;
 
-    const createQuery =
-      <K extends ProcedurePaths<Router<R, P>, "query">>(name: K) =>
-      (payload: GetInputType<K, Router<R, P>>, options: Parameters<typeof get>[1]) => {
-        const endpoint = `${routerURL}.${name}`;
-        if (payload === NONE) {
-          return get(endpoint, options);
-        }
+    if (args.length === 2) {
+      input = args[0];
+      if (isOptions(args[1])) {
+        opts = args[1];
+      }
+    }
+    if (args.length === 1) {
+      if (isOptions(args[0])) {
+        opts = args[0];
+      } else {
+        input = args[0];
+      }
+    }
 
-        return get(
-          `${endpoint}?input=${encodeURIComponent(SuperJSON.stringify(payload))}`,
-          options,
-        );
-      };
+    let trimmedOpts: Params | undefined;
+    if (opts) {
+      const { [OPTION]: _, ...rest } = opts;
+      trimmedOpts = rest;
+    }
 
-    const createMutation =
-      <K extends ProcedurePaths<Router<R, P>, "mutation">>(name: K) =>
-      (payload: GetInputType<K, Router<R, P>>, options: Parameters<typeof post>[2]) => {
-        const endpoint = `${routerURL}.${name}`;
-        if (payload === NONE) {
-          return post(endpoint, undefined, options);
-        }
+    let serializedInput: string | undefined;
+    if (input !== NONE) {
+      if (isCombinedDataTransformer(transformer)) {
+        serializedInput = JSON.stringify(transformer.input.serialize(input));
+      } else {
+        serializedInput = JSON.stringify(transformer.serialize(input));
+      }
+    }
 
-        return post(endpoint, SuperJSON.stringify(payload), options);
-      };
+    if (property === "query" && serializedInput === undefined) {
+      return http.get(url, trimmedOpts);
+    }
+    if (property === "query") {
+      return http.get(`${url}?input=${serializedInput}`, opts);
+    }
 
-    return {
-      createQuery,
-      createMutation,
-    };
+    return http.post(url, serializedInput, trimmedOpts);
   };
+};
 
+const createProxyHandler = (
+  {
+    url,
+    transformer = defaultTransformer,
+  }: {
+    url: string;
+    transformer?: CombinedDataTransformer | DataTransformer;
+  },
+  path = "",
+) => {
   return {
-    createRouter,
+    // @ts-expect-error: Proxy is not typed
+    get(_: unknown, property: string) {
+      const base = new URL(path, url).toString();
+      const resource = path === "" ? property : path + "." + property;
+
+      if (property === "query" || property === "mutate") {
+        const target = createFunctionHandler(base, property, transformer);
+        const handler = createProxyHandler({ url, transformer }, resource);
+
+        return new Proxy(target, handler);
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return createClientImplementation({ url, transformer }, resource);
+    },
   };
+};
+
+// @ts-expect-error: Proxy is not typed
+const createClientImplementation = (
+  {
+    url,
+    transformer = defaultTransformer,
+  }: {
+    url: string;
+    transformer?: CombinedDataTransformer | DataTransformer;
+  },
+  path = "",
+) => {
+  const handler = createProxyHandler({ url, transformer }, path);
+
+  return new Proxy({}, handler);
+};
+
+export const createClient = <R extends AnyRouter>(
+  url: string,
+  transformer?: CombinedDataTransformer | DataTransformer,
+) => {
+  // Ensure that the URL ends with a slash.
+  if (!url.endsWith("/")) {
+    url.concat("/");
+  }
+
+  return createClientImplementation({ url, transformer }) as Client<R>;
 };
